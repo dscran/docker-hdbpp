59ab7d33c59d84afaa51a606746a36b585e80473
Michael Schneider <mschneid@mbi-berlin.de>
2022-02-27 17:15:05 +0100

    move packge install to separate script

--- tango-hdbpp-ds/setup/020-install-hdbpp-ds.sh
+++ tango-hdbpp-ds/setup/020-install-hdbpp-ds.sh
@@ -3,12 +3,18 @@
 # disable automatic service start during build
 mv /usr/sbin/policy-rc.d.disabled /usr/sbin/policy-rc.d
 
+apt-get update
+apt-get install -y --no-install-recommends \
+    tango-starter \
+    libmariadb3
+
 dpkg -i /*deb
 ldconfig
 /lib/systemd/systemd-sysv-install enable tango-starter
 
 apt-get clean
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+rm /*deb
 /usr/local/bin/docker-wipelogs
 
 # enable service starts again
