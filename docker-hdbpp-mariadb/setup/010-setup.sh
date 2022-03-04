724c81ce9572e99ea801256344ef25d367202e24
Michael Schneider <mschneid@mbi-berlin.de>
2022-03-04 18:01:23 +0100

    working separate DS

--- docker-hdbpp-mariadb/setup/010-setup.sh
+++ docker-hdbpp-mariadb/setup/010-setup.sh
@@ -8,13 +8,13 @@ apt-get install -y --no-install-recommends \
     mariadb-server \
 
 # perform mariadb_secure_install actions
-# mysql -fu root < /mariadb_secure_installation.sql
+mysql -fu root < /mariadb_secure_installation.sql
 
 # create database and default users
-# mysql -u root < /create_hdbpp_users.sql
+mysql -u root < /create_hdbpp_users.sql
 
 # init hdbpp database
-# mysql -u root --database=hdbpp < /create_hdb++_mysql_innodb_json.sql
+mysql -u root --database=hdbpp < /create_hdb++_mysql_innodb_json.sql
 
 # enable network access to database by replacing localhost bind-address
 # (127.0.0.1 -> 0.0.0.0)
