4a2c91bd74a740015385d32a691bcb3e951e2de7
Michael Schneider <mschneid@mbi-berlin.de>
2022-03-06 12:23:55 +0100

    remove mysql image

--- docker-hdbpp-mysql/setup/create_hdbpp_users.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER 'hdbpprw'@'%' IDENTIFIED BY 'hdbpprw';
-CREATE USER 'hdbppro'@'%' IDENTIFIED BY 'hdbppro';
-
-GRANT ALL ON hdbpp.* TO 'hdbpprw'@'%';
-GRANT SELECT ON hdbpp.* TO 'hdbppro'@'%';
-
-FLUSH PRIVILEGES;
