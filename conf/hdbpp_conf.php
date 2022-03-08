defe5ee873302f91001baac7132b9d8e4ee8e808
MBI (curie.maxi.lab) <labuser@curie.maxi.lab>
2022-03-08 14:59:37 +0100

    prepare eGiga2m

---
+++ conf/hdbpp_conf.php
@@ -0,0 +1,18 @@
+<?php
+define("HOST", "hdbpp-db.hdbnet");
+define("USERNAME", "hdbppro");
+define("PASSWORD", "hdbppro");
+define("PORT", 3306);
+define("DB", "hdbpp");
+define("DBTYPE", "hdbpp");
+define("LOG_REQUEST", "../../log/hdbpp_1.log");
+
+// if you wont to hide a part of your domain edit the following array
+$skipdomain = array('tango://curie.maxi.lab:10000/'=>'');
+
+// define("ALARM_HOST", "host");
+// define("ALARM_USER", "username");
+// define("ALARM_PASSWORD", "password");
+// define("ALARM_DB", "db");
+
+?>
